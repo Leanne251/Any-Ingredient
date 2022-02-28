cbc9518a8ca0c90d4fdf81495fd6a04663a61333
Leanne251 <leannesmith251@gmail.com>
2022-02-28 18:16:00 +0000

    delete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,44 @@
 import Questions from '../Questions/Questions';
+import { darkMode, lightMode } from '../../styles/theme';
+import { React, useState } from 'react';
+import './App.css';
+import { ThemeProvider, IconButton } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { createTheme } from '@material-ui/core/styles';
+import Brightness3Icon from '@material-ui/icons/Brightness3';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
+import NavBar from '../NavBar/NavBar';
+
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+	root: {
+		marginBottom: theme.spacing(2),
+		flexGrow: 1
+	},
+	title: {
+		flexGrow: 1
+	}
+}));
+
 function App() {
-	return <Questions />;
+	const [ theme, setTheme ] = useState(true);
+	const classes = useStyles();
+	const icon = !theme ? <Brightness7Icon /> : <Brightness3Icon />;
+	const appliedTheme = createTheme(theme ? lightMode : darkMode);
+
+	return (
+		<ThemeProvider theme={appliedTheme}>
+			<NavBar />
+			<div className={classes.root}>
+				<IconButton edge="end" color="inherit" aria-label="mode" onClick={() => setTheme(!theme)}>
+					{icon}
+				</IconButton>
+
+				<Questions />
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
